631b255ca05d3ca0846085c004c00ab9c566d8d3
TrinhThiVan26 <trinhthivan26122006@gmail.com>
2025-10-03 00:03:38 +0700

    Update TetrisGrid.java

--- oop-main/HW2/src/main/java/TetrisGrid.java
+++ oop-main/HW2/src/main/java/TetrisGrid.java
@@ -2,28 +2,81 @@
 // TetrisGrid encapsulates a tetris board and has
 // a clearRows() capability.
 
+import java.util.Scanner;
+
 public class TetrisGrid {
-	
-	/**
-	 * Constructs a new instance with the given grid.
-	 * Does not make a copy.
-	 * @param grid
-	 */
-	public TetrisGrid(boolean[][] grid) {
-	}
-	
-	
-	/**
-	 * Does row-clearing on the grid (see handout).
-	 */
-	public void clearRows() {
-	}
-	
-	/**
-	 * Returns the internal 2d grid array.
-	 * @return 2d grid array
-	 */
-	boolean[][] getGrid() {
-		return null; // YOUR CODE HERE
-	}
+    private boolean[][] grid;
+
+    /**
+     * Constructs a new instance with the given grid.
+     */
+    public TetrisGrid(boolean[][] grid) {
+        this.grid = grid;
+    }
+
+
+    /**
+     * Does row-clearing on the grid (see handout).
+     */
+    public void clearRows() {
+        int fellrow = -1;
+        for (int i = grid.length - 1; i >= 0; i--) {
+            boolean full = true;
+            for (int j = 0; j < grid[i].length; j++) {
+                if (!grid[i][j]) {
+                    full = false;
+                    break;
+                }
+            }
+
+            if (full) {
+                if (fellrow == -1) {
+                    fellrow = i;
+                }
+            } else if (fellrow != -1) {
+                for (int k = 0; k < grid[i].length; k++) {
+                    grid[fellrow][k] = grid[i][k];
+                    grid[i][k] = false;
+                }
+                fellrow--;
+            }
+        }
+
+        while (fellrow >= 0) {
+            for (int k = 0; k < grid[0].length; k++) {
+                grid[fellrow][k] = false;
+            }
+            fellrow--;
+        }
+    }
+
+
+    /**
+     * Returns the internal 2d grid array.
+     */
+    boolean[][] getGrid() {
+        return this.grid;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n, m;
+        n = sc.nextInt();
+        m = sc.nextInt();
+        boolean[][] grid = new boolean[n][m];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                grid[i][j] = sc.nextBoolean();
+            }
+        }
+        TetrisGrid tg = new TetrisGrid(grid);
+        tg.clearRows();
+        boolean[][] res = tg.getGrid();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.print(res[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 }
